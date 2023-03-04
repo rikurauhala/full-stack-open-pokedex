d5d4f0cb2a3a468b6bb6f6dc76046386313200a3
Riku Rauhala <riku@rauhala.io>
2023-03-04 16:01:28 +0200

    Create another Cypress test

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('clicking a link works as expected', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
+  })
 })
