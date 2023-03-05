d06ca2448b426936345843bf770bbf00262adc1e
Riku Rauhala <riku@rauhala.io>
2023-03-05 20:52:24 +0200

    Update the health check script

--- health_check.sh
+++ health_check.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-exit 0
+STATUS=$(curl --silent https://pokedex-cicd.fly.dev/health)
+
+if [ "$STATUS" == "ok" ]; then
+  printf "Health check ok"
+  exit 0
+fi
+
+printf "Health check failed"
+exit 1
