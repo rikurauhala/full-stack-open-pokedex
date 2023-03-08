906dbeeedc15e9aa9dd0d9112d6d93245d54bded
Riku Rauhala <riku@rauhala.io>
2023-03-08 19:37:14 +0200

    Fix app.js

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('server started on port', PORT)
+})
+
 app.get('/health', (req, res) => {
   res.send('ok')
 })
