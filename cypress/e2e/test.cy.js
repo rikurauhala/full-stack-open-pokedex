60bd68a24c96d0d411977c0a761135d4dc268652
Riku Rauhala <riku@rauhala.io>
2023-03-08 18:54:22 +0200

    Fix the pipeline

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -7,7 +7,7 @@ describe('Pokedex', function() {
   it('clicking a link works as expected', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur').click()
-    cy.contains('overgrowww')
+    cy.contains('overgrow')
     cy.contains('chlorophyll')
   })
 })
