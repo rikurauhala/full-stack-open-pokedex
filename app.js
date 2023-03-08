9f4ec50000b0bc023def3eff5e729b2877a9cf21
Riku Rauhala <riku@rauhala.io>
2023-03-08 19:32:50 +0200

    Schedule the health check every 15 minutes

--- app.js
+++ app.js
@@ -6,11 +6,6 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log('server started on port', PORT)
-})
-
 app.get('/health', (req, res) => {
   res.send('ok')
 })
